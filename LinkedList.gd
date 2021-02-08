41452266eac158bccd03e0eba029376765b8e2d3
simon1245 <37098385+simon1245@users.noreply.github.com>
2021-02-08 15:11:22 +0800

    linked list

--- LinkedList.gd
+++ LinkedList.gd
@@ -2,85 +2,65 @@ extends Reference
 
 class LinkedListItem:
     extends Reference
+    var head;
+    var next = node.new(null,null);
     
-    var next = null
-    var previous = null
-    var data = {}
-    
-    func _init(v):
-        data = v
-    
-    func link(other):
-        other.previous = self
-        next = other
-    
-    func unlink():
-        var _next = next
-        var _previous = previous
-        if _previous:
-            _previous.next = next
-        if _next:
-            _next.previous = previous
-
-var _tail = null
-var _head = _tail
-var _len = 0
-
-func size():
-    return _len
-
-func push_back(val):
-    if _len == 0:
-        _head = LinkedListItem.new(val)
-        _tail = _head
-    else:
-        var new_head = LinkedListItem.new(val)
-        _head.link(new_head)
-        _head = new_head
-    _len += 1
-
-func push_front(val):
-    if _len == 0:
-        _head = LinkedListItem.new(val)
-        _tail = _head
-    else:
-        var new_tail = LinkedListItem.new(val)
-        new_tail.link(_tail)
-        _tail = new_tail
-    _len += 1
-
-func pop_back():
-    if _len == 0:
-        return null
-    else:
-        var result = _head.data
-        _head = _head.previous
-        _len -= 1
-        return result
-
-func pop_front():
-    if _len == 0:
-        return null
-    else:
-        var result = _tail.data
-        _tail = _tail.next
-        _len -= 1
-        return result
-
-func pop_best(better_func):
-    if _len == 0:
-        return null
-    else:
-        var current_best = _tail
-        var next = _tail.next
-        while next:
-            if not better_func.call_func(current_best.data, next.data):
-                current_best = next
-            next = next.next
-        if _head == current_best:
-            return pop_back()
-        if _tail == current_best:
-            return pop_front()
+    func _init():
+        head =  node.new(null,null);
+        
+    func insertNode(Index,x):
+        if(Index<0):
+            return null;            
+        elif(Index==0 && next.data ==null):
+            var newnode = node.new(Index,x);
+            newnode.data = x;
+            next = newnode;
+        elif(Index==0):
+            var newnode = node.new(Index,x);
+            newnode.data = x;
+            newnode.nextNode = next;
+            next = newnode;
         else:
-            current_best.unlink()
-            return current_best.data
+            var currIndex = 1;
+            var currNode = next;
+            while (Index > currIndex):
+                currIndex+=1;
+                currNode = currNode.nextNode;
+            var newnode = node.new(Index,x);
+            newnode.data = x;
+            newnode.nextNode = currNode.nextNode;
+            newnode.preNode = currNode;
+            currNode.nextNode = newnode;
+   
+    func deleteNode(Index):
+        var currNode = next;
+        var preNode;
+        while (currNode.index != Index && currNode.nextNode!=null):
+            preNode = currNode;
+            currNode = currNode.nextNode;
+        preNode.next = currNode.nextNode;
+        currNode = currNode.nextNode;
+        currNode.preNode = preNode;
+                
+    func printall():
+        if(next==null):
+            return null;
+        var currNode = next;
+        var stra = "1. " + str(currNode.data);
+        var count = 1;
+        while (currNode.nextNode != null):
+            currNode = currNode.nextNode
+            count+=1;
+            stra =  stra + '\n' + str(count) + ". " + str(currNode.data);            
+        return stra;
+        
+class node:
+    var nextNode = null;
+    var preNode = null;
+    var data ;
+    var index;
+    
+    func _init(number,x):
+        var data = x;
+        var index = number;
+
